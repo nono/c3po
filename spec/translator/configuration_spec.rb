71400ae4fb976be8c921631590a1ec23a27ddf5c
chatgris <jboyer@af83.com>
2011-07-27 09:30:01 +0200

    Better spec organization.

---
+++ spec/translator/configuration_spec.rb
@@ -0,0 +1,57 @@
+# encoding: utf-8
+require 'spec_helper'
+
+describe C3po::Translator::Configuration do
+  context 'google as provider' do
+
+    before do
+      C3po.configure do |config|
+        config.provider = :google
+        config.google_api_key = 'MYAPIKEY'
+      end
+    end
+
+    it 'Configuration should be autoload' do
+      defined?(C3po::Translator::Configuration).should be_true
+    end
+
+    it 'should respond_to configure' do
+      C3po.respond_to?(:configure).should be_true
+    end
+
+    it 'should have a provider method' do
+      C3po::Translator::Configuration.provider.should eq(:google)
+    end
+
+    it 'should have a google_api_key method' do
+      C3po::Translator::Configuration.google_api_key.should eq('MYAPIKEY')
+    end
+  end
+
+  context 'bing as provider' do
+
+    before do
+      C3po.configure do |config|
+        config.provider = :bing
+        config.google_api_key = 'MYAPIKEY'
+      end
+    end
+
+    it 'Configuration should be autoload' do
+      defined?(C3po::Translator::Configuration).should be_true
+    end
+
+    it 'should respond_to configure' do
+      C3po.respond_to?(:configure).should be_true
+    end
+
+    it 'should have a provider method' do
+      C3po::Translator::Configuration.provider.should eq(:bing)
+    end
+
+    it 'should have a google_api_key method' do
+      C3po::Translator::Configuration.google_api_key.should eq('MYAPIKEY')
+    end
+  end
+
+end
---
+++ spec/translator/google_spec.rb
@@ -0,0 +1,22 @@
+# encoding: utf-8
+require 'spec_helper'
+
+describe C3po::Translator::Google do
+  let(:translator) { C3po.new('to be translated')}
+  let(:json_response) {File.open(file_path('multiple.json')).read}
+
+  before do
+    C3po.configure do |config|
+      config.provider = :google
+      config.google_api_key = 'MYAPIKEY'
+    end
+  end
+
+  it 'should build a query hash' do
+    translator.build_query(:fr, :en).should eq({:key=>"MYAPIKEY", :q=>"to be translated", :source=>'fr', :target=> 'en'})
+  end
+
+  it 'should read json response' do
+    translator.parse(json_response).should eq('Hallo Welt')
+  end
+end
